47b233c34c97f35c99905aece18557d62669683b
Charro Gruver <charro_gruver@yahoo.com>
2021-11-10 07:42:23 -0500

    fix mirror script

--- bin/mirrorOkdRelease.sh
+++ bin/mirrorOkdRelease.sh
@@ -1,27 +1,51 @@
 #!/bin/bash
 
-OKD_REGISTRY=${OKD_STABLE_REGISTRY}
-NIGHTLY=false
-
 for i in "$@"
 do
 case $i in
-    -n|--nightly)
-    NIGHTLY=true
-    shift
+    -c=*|--config=*)
+      CONFIG_FILE="${i#*=}"
+      HOST_CONFIG=${CONFIG_FILE}
+      shift
+    ;;
+    -d=*|--domain=*)
+      SUB_DOMAIN="${i#*=}"
+      shift
     ;;
     *)
-          # put usage here:
+          # Put usage here:
     ;;
 esac
 done
 
-if [[ ${NIGHTLY} == "true" ]]
+DONE=false
+DOMAIN_COUNT=$(yq e ".sub-domain-configs" ${CONFIG_FILE} | yq e 'length' -)
+let i=0
+while [[ i -lt ${DOMAIN_COUNT} ]]
+do
+  domain_name=$(yq e ".sub-domain-configs.[${i}].name" ${CONFIG_FILE})
+  if [[ ${domain_name} == ${SUB_DOMAIN} ]]
+  then
+    INDEX=${i}
+    DONE=true
+    break
+  fi
+  i=$(( ${i} + 1 ))
+done
+if [[ ${DONE} == "false" ]]
 then
-  OKD_REGISTRY=${OKD_NIGHTLY_REGISTRY}
+  echo "Domain Entry Not Found In Config File."
+  exit 1
 fi
+
+LAB_DOMAIN=$(yq e ".domain" ${CONFIG_FILE})
+SUB_DOMAIN=$(yq e ".sub-domain-configs.[${INDEX}].name" ${CONFIG_FILE})
+CLUSTER_CONFIG=$(yq e ".sub-domain-configs.[${INDEX}].cluster-config-file" ${CONFIG_FILE})
+DOMAIN="${SUB_DOMAIN}.${LAB_DOMAIN}"
+LOCAL_REGISTRY=$(yq e ".local-registry" ${CLUSTER_CONFIG})
+OKD_REGISTRY=$(yq e ".remote-registry" ${CLUSTER_CONFIG})
+PULL_SECRET=$(yq e ".secret-file" ${CLUSTER_CONFIG})
 
 OKD_RELEASE=$(oc version --client=true | cut -d" " -f3)
 
-oc adm -a ${LOCAL_SECRET_JSON} release mirror --from=${OKD_REGISTRY}:${OKD_RELEASE} --to=${LOCAL_REGISTRY}/${OKD_RELEASE} --to-release-image=${LOCAL_REGISTRY}/${OKD_RELEASE}:${OKD_RELEASE}
-
+oc adm -a ${PULL_SECRET} release mirror --from=${OKD_REGISTRY}:${OKD_RELEASE} --to=${LOCAL_REGISTRY}/${OKD_RELEASE} --to-release-image=${LOCAL_REGISTRY}/${OKD_RELEASE}:${OKD_RELEASE}
